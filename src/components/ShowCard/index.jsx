3137751b75e04fce1f61402c0967984cca102dae
agiskorska <skorskaga@gmail.com>
2022-08-23 12:02:30 +0100

    condtitionally render picture

--- src/components/ShowCard/index.jsx
+++ src/components/ShowCard/index.jsx
@@ -4,8 +4,9 @@ const ShowCard = ({data}) => {
     console.log(data)
   return (
     <div className='showCard'>
+        <img src={data["image"] ? data["image"]["medium"] : ""} className='' alt="an image of the film" />
         <h2>{data.name}</h2>
-
+        <p>{data.summary}</p>
     </div>
   )
 }
